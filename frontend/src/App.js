ac98a71b9f172cce58182b3401bf0221a35fc251
jachlac <108031655+jachlac@users.noreply.github.com>
2022-10-22 18:11:08 -0300

    se agrego enlace a estilo react-bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { LoremIpsum } from 'react-lorem-ipsum';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Header from './components/layout/Header';
 import Nav from './components/layout/Nav';
