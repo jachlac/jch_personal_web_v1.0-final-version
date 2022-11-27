99895f8787c5b1dfc6a12e997d009573e9452710
jachlac <108031655+jachlac@users.noreply.github.com>
2022-11-26 21:27:00 -0300

    envio de correos completo

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var updatesModel = require('../models/updatesModel')
 var cloudinary = require('cloudinary').v2;
+var nodemailer = require('nodemailer');
+const { getMaxListeners } = require('../models/db');
 
 router.get('/updates', async function (req, res, next) {
 
@@ -27,6 +29,31 @@ router.get('/updates', async function (req, res, next) {
   });
 
   res.json(updates); // aca la respuesta es un JSON, en lugar de render, para react es la forma de recibirlo y trabajarlo dentro del diseño
+});
+
+
+router.post('/contact', async (req, res) => {
+  const mail = {
+    to: 'cryptostate.1980@gmail.com',
+    subject: 'Web contact',
+    html: ` ${req.body.name} had contact through the website, and wants more information to this email: ${req.body.email} <br> Also made the following comment: ${req.body.message} <br> His phone is: ${req.body.phone}`
+  }
+
+  var transport = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+  });
+
+  await transport.sendMail(mail)
+
+  res.status(201).json({
+    error: false,
+    message: 'message send' //mando la propiedad de mensaje para asegurarme que se envio correctamente
+  });
 });
 
 module.exports = router;
--- frontend/src/pages/ContactoPage.js
+++ frontend/src/pages/ContactoPage.js
@@ -1,51 +1,91 @@
 import React from 'react';
 import '../styles/Pages/Contacto.css';
+import { useState } from 'react'; //trabajamos con estados
+import axios from 'axios';
 
 const Contact = (props) => {
+
+    const initialForm = {
+        name: '',
+        email: '',
+        phone: '',
+        message: '',
+    }
+
+    const [sending, setSending] = useState(false);
+    const [msg, setMsg] = useState('');
+    const [formData, setFormData] = useState(initialForm);
+
+    const handleChange = e => { //como vamos capturando los datos
+        const { name, value } = e.target;
+        setFormData(oldData => ({
+            ...oldData,
+            [name]: value
+        }));
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        setMsg('');
+        setSending(true)
+        const response = await axios.post('http://localhost:3000/api/contact', formData);
+        setSending(false);
+        setMsg(response.data.message);
+        if (response.data.error === false) {
+            setFormData(initialForm)
+        }
+    }
+
+
+
     return (
         <main className='holder contacto'>
             <div>
                 <h2>Fast Contact</h2>
-                <form action='' method='' className='formulario'>
+                <form action='/contact' method='' onSubmit={handleSubmit} className='formulario'>
                     <p>
                         <label for="name">Name</label>
-                        <input type="text" name="" />
+                        <input type="text" name="name" value={formData.name} onChange={handleChange} />
                     </p>
                     <p>
                         <label for="email">Email</label>
-                        <input type="text" name="" />
+                        <input type="text" name="email" value={formData.email} onChange={handleChange} />
                     </p>
                     <p>
                         <label for="phone">Phone</label>
-                        <input type="text" name="" />
+                        <input type="text" name="phone" value={formData.phone} onChange={handleChange} />
                     </p>
                     <p>
                         <label for="message">Message</label>
-                        <textarea name=""></textarea>
+                        <textarea name="message" value={formData.message} onChange={handleChange}></textarea>
                     </p>
                     <p class="acciones">
                         <input type="submit" name="" />
                     </p>
                 </form>
+                {sending ? <p>sending...</p> : null}
+                {msg ? <p>{msg}</p> : null}
+
+
             </div>
             <div className='datos'>
                 <h2>Other ways to get in touch</h2>
                 <p>You can also contact us by:</p>
                 <ul className='logos'>
                     <div>
-                    <img src="./images/pages/contacto/twitter.png" width={50} alt="twitter"></img>
+                        <img src="./images/pages/contacto/twitter.png" width={50} alt="twitter"></img>
                     </div>
                     <div>
-                    <img src="./images/pages/contacto/gmail.png" width={50} alt="gmail"></img>
+                        <img src="./images/pages/contacto/gmail.png" width={50} alt="gmail"></img>
                     </div>
                     <div>
-                    <img src="./images/pages/contacto/linkedin.png" width={50} alt="linkedin"></img>
+                        <img src="./images/pages/contacto/linkedin.png" width={50} alt="linkedin"></img>
                     </div>
-                   
+
 
                 </ul>
             </div>
         </main>
-        );
-    }
-    export default Contact;
+    );
+}
+export default Contact;
